c64e9e64f4127a39e3df730bd14c9f722d46560d
andreykeyr <127974278+Andreykeyr@users.noreply.github.com>
2023-09-14 10:22:48 +0300

    updateAll
    Я понял в чем проблема :)))

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
 
-for(let i=0;i<galleryItems.length;i++){
-   let li = document.createElement('li')
-   let img = `<img alt=${galleryItems[i].description} src=${galleryItems[i].preview}>`
-   li.innerHTML = img
-   const instance = basicLightbox.create(`
-   <img alt=${galleryItems[i].description} src=${galleryItems[i].original} width="800" height="600">
-   `)
-   li.addEventListener('click',()=>{
-      instance.show()
-   })
-   document.querySelector("ul").append(li)
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
+  }
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
+}
+
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
 }
